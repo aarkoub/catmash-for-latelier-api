fbce28da6ddb7a5036918550990ced483e390b07
aarkoub <aarkoub@github.com>
2021-05-08 23:29:28 +0200

    Refactor cat service test with mockito [domain]

--- src/test/java/aarkoub/catmash/domain/cat/service/CatServiceTest.java
+++ src/test/java/aarkoub/catmash/domain/cat/service/CatServiceTest.java
@@ -2,43 +2,30 @@ package aarkoub.catmash.domain.cat.service;
 
 import aarkoub.catmash.db.cat.ICatRepository;
 import aarkoub.catmash.domain.cat.Cat;
-import aarkoub.catmash.domain.cat.service.CatService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@SpringBootTest
 public class CatServiceTest {
 
-    private class MockedCatRepository implements ICatRepository {
+    @MockBean
+    ICatRepository mockRepository;
 
-        @Override
-        public long addCat(Cat cat) {
-            return 1;
-        }
-
-        @Override
-        public Cat voteForCat(long id) throws Exception {
-            return new Cat(1, "fake_url", 5);
-        }
-
-        @Override
-        public List<Cat> getAllCats() {
-            Cat c = new Cat(1, "fake_url", 5);
-            Cat c2 = new Cat(2, "fake_url", 5);
-            List<Cat> cats = new ArrayList<>();
-            cats.add(c);
-            cats.add(c2);
-            return cats;
-        }
-    }
-
-    CatService catService = new CatService(new MockedCatRepository());
+    @Autowired
+    ICatService catService ;
 
     @Test
     public void testAddCat(){
+
         Cat cat = new Cat(2L, "fake_url", 5);
+        Mockito.when(mockRepository.addCat(cat)).thenReturn(1L);
         Assertions.assertEquals(1, catService.addCat(cat));
     }
 
@@ -47,11 +34,14 @@ public class CatServiceTest {
         List<Cat> cats = new ArrayList<>();
         cats.add(new Cat(1, "fake_url", 5));
         cats.add(new Cat(2, "fake_url", 5));
+        Mockito.when(mockRepository.getAllCats()).thenReturn(cats);
         Assertions.assertEquals(cats, catService.getAllCats());
     }
 
     @Test
     public void testVoteForCat() throws Exception {
+        Cat cat = new Cat(1, "fake_url", 5);
+        Mockito.when(mockRepository.voteForCat(2)).thenReturn(cat);
         Assertions.assertEquals(5, catService.voteForCat(2).getNbVotes());
     }
 
