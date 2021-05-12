b3250f7cc77c0b3958a306193cac3f56a4d1d7a3
aarkoub <aarkoub@github.com>
2021-05-13 01:18:15 +0200

    Add API paths [api][test]
    TODO: handle http error

--- src/main/java/aarkoub/catmash/API.java
+++ src/main/java/aarkoub/catmash/API.java
@@ -3,13 +3,22 @@ package aarkoub.catmash;
 import aarkoub.catmash.db.cat.importation.CatDataLoader;
 import aarkoub.catmash.domain.cat.Cat;
 import aarkoub.catmash.domain.cat.service.ICatService;
+import aarkoub.catmash.domain.catmatch.CatMatch;
+import aarkoub.catmash.domain.catmatch.service.ICatMatchService;
+import aarkoub.catmash.domain.user.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @SpringBootApplication
@@ -19,17 +28,63 @@ public class API {
     ICatService catService;
 
     @Autowired
+    IUserService userService;
+
+    @Autowired
+    ICatMatchService catMatchService;
+
+    @Autowired
     CatDataLoader catDataLoader;
+
+
+    public @Nullable UUID getUserId(HttpServletRequest request){
+        UUID userId = null;
+        Cookie[] cookies = request.getCookies();
+        if(cookies!=null) {
+            for (int i = 0; i < cookies.length; i++) {
+                if (cookies[i].getName().equals("userId")) {
+                    userId = UUID.fromString(cookies[i].getValue());
+                    break;
+                }
+            }
+        }
+        return userId;
+    }
 
     @RequestMapping("/")
     @ResponseBody
-    String index() {
-        return "Hello World!";
+    Cookie[] index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        return cookies;
     }
 
     @GetMapping("/cats")
+    @ResponseBody
     List<Cat> getAllCats() {
         return catService.getAll();
+    }
+
+    @GetMapping("/cats/match")
+    @ResponseBody
+    CatMatch generateCatMatch(HttpServletRequest request){
+        return catMatchService.generateMatch(getUserId(request));
+    }
+
+    @PostMapping(path="/cats/match/vote", consumes = "application/json;charset=UTF-8")
+    CatMatch voteForCat( HttpServletRequest request, @RequestBody Map<String, Long> catIds){
+        try {
+           return catMatchService.changeVote(getUserId(request), catIds.get("catId1"), catIds.get("catId2"),
+                   catIds.get("catIdVoted"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @GetMapping("/user")
+    @ResponseBody
+    void retrieveUser(HttpServletRequest request, HttpServletResponse response){
+        response.addCookie(new Cookie("userId", userService.retrieveUser(getUserId(request)).getId().toString()));
     }
 
     @RequestMapping("/load")
--- src/test/java/aarkoub/catmash/APITests.java
+++ src/test/java/aarkoub/catmash/APITests.java
@@ -1,13 +1,113 @@
 package aarkoub.catmash;
 
+import static org.hamcrest.CoreMatchers.*;
+
+import aarkoub.catmash.domain.cat.Cat;
+import aarkoub.catmash.domain.catmatch.CatMatch;
+import aarkoub.catmash.domain.user.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.servlet.http.Cookie;
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.cookie;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+@AutoConfigureTestEntityManager
+@Transactional
 class APITests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Test
+    void getAllCats() throws Exception {
+        testEntityManager.persist(new Cat("fake_url", 0));
+        testEntityManager.persist(new Cat("fake_url", 0));
+        mockMvc.perform(get("/cats"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id", is(1)))
+                .andExpect(jsonPath("$[1].id", is(2)))
+                .andExpect(jsonPath("$.length()", is(2)));
+    }
+
+    @Test
+    void retrieveUser() throws Exception {
+        mockMvc.perform(get("/user"))
+                .andExpect(status().isOk())
+                .andExpect(cookie().exists("userId"));
+
+        UUID id = testEntityManager.persist(new User()).getId();
+        Cookie cookie = new Cookie("userId", id.toString());
+        mockMvc.perform(get("/user").cookie(cookie))
+                .andExpect(status().isOk())
+                .andExpect(cookie().value("userId", id.toString()));
+    }
+
+    @Test
+    void generateMatch() throws Exception {
+
+        testEntityManager.persist(new Cat("fake_url", 5));
+        testEntityManager.persist(new Cat("fake_url", 3));
+        UUID id = testEntityManager.persist(new User()).getId();
+        Cookie cookie = new Cookie("userId", id.toString());
+
+        mockMvc.perform(get("/cats/match").cookie(cookie))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.user.id", is(id.toString())))
+                .andExpect(jsonPath("$.cat1.id", anyOf(is(1), is(2))))
+                .andExpect(jsonPath("$.cat2.id", anyOf(is(1), is(2))))
+                .andExpect(jsonPath("$.catVoted", nullValue()));
+    }
+
+    @Test
+    void voteForCat() throws Exception {
+        Cat cat1 = testEntityManager.persist(new Cat("fake_url", 5));
+        Cat cat2 = testEntityManager.persist(new Cat("fake_url", 3));
+        User user = testEntityManager.persist(new User());
+        CatMatch match = testEntityManager.persist(new CatMatch(user, cat1, cat2));
+        testEntityManager.flush();
+        Cookie cookie = new Cookie("userId", user.getId().toString());
+        mockMvc.perform(post("/cats/match/vote")
+                .cookie(cookie)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"catId1\":" + match.getCat1().getId() + "," +
+                        "\"catId2\":" + match.getCat2().getId() + ", " +
+                        "\"catIdVoted\":" + match.getCat1().getId() + "}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.cat1.nbVotes", is(6)))
+                .andExpect(jsonPath("$.cat2.nbVotes", is(3)));
+
+        mockMvc.perform(post("/cats/match/vote")
+                .cookie(cookie)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"catId1\":" + match.getCat1().getId() + "," +
+                        "\"catId2\":" + match.getCat2().getId() + ", " +
+                        "\"catIdVoted\":" + match.getCat2().getId() + "}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.cat1.nbVotes", is(5)))
+                .andExpect(jsonPath("$.cat2.nbVotes", is(4)));
+
+    }
 
 }
