80e566021725d86649ee5be90bbf9310af6c1616
amel <aarkoub@github.com>
2021-05-25 12:28:09 +0200

    Fix security user cookie for chrome : samesite = none [security]

--- src/main/java/aarkoub/catmash/WebSecurityConfig.java
+++ src/main/java/aarkoub/catmash/WebSecurityConfig.java
@@ -3,12 +3,34 @@ package aarkoub.catmash;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.filter.GenericFilterBean;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.List;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+
+
+    public class SameSiteFilter extends GenericFilterBean {
+        @Override
+        public void doFilter (ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+            HttpServletResponse resp = (HttpServletResponse) response;
+            resp.setHeader("Set-Cookie", "HttpOnly; SameSite=strict");
+                    chain.doFilter(request, response);
+        }
+    }
+
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -20,5 +42,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             cors.setAllowedHeaders(List.of("*"));
             return cors;
         }).and().csrf().disable();
+
+        http.addFilterAfter(new SameSiteFilter(), BasicAuthenticationFilter.class);
     }
 }
